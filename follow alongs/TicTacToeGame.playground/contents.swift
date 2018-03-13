223d1e896fe74e2c22b33afec3cf0ab3dad87e71
David Fisher <fisherds@rose-hulman.edu>
2018-03-13 12:06:04 -0400

    Construct and display a TTT game

--- follow alongs/TicTacToeGame.playground/contents.swift
+++ follow alongs/TicTacToeGame.playground/contents.swift
@@ -1,11 +1,41 @@
+class TicTacToeGame: CustomStringConvertible {
+  enum GameState: String {
+    case xTurn = "X's Turn"
+    case oTurn = "O's Turn"
+    case xWon = "X Wins!"
+    case oWon = "O Wins!"
+    case tie = "Tie Game"
+  }
+  enum MarkType: String {
+    case x = "X"
+    case o = "O"
+    case none = "-"
+  }
 
+  var gameState: GameState
+  var gameBoard: [MarkType]
 
-// TODO: Create the TicTacToeGame class
+  init() {
+    gameState = .xTurn
+    gameBoard = [MarkType](repeating: .none, count: 9)
+  }
 
+  func getGameString(_ indices: [Int] = [0, 1, 2, 3, 4, 5, 6, 7, 8]) -> String {
+    var gameString = ""
+    for index in indices {
+      gameString += gameBoard[index].rawValue
+    }
+    return gameString
+  }
+
+  var description: String {
+    return "\(gameState.rawValue)  Board: \(getGameString())"
+  }
+}
 
 
 /* ----------------- Official Playground testing ----------------- */
-//var game = TicTacToeGame()
+var game = TicTacToeGame()
 //game.pressedSquare(0)
 //game.pressedSquare(1)
 //game.pressedSquare(3)
@@ -13,17 +43,17 @@
 //game.pressedSquare(6)
 
 
-//var game2 = TicTacToeGame()
-//game2.gameBoard = [.x, .x, .o,
-//                .none, .none, .none,
-//                .o, .none, .none]
+var game2 = TicTacToeGame()
+game2.gameBoard = [.x, .x, .o,
+                .none, .none, .none,
+                .o, .none, .none]
 //game2.pressedSquare(8)
 //game2.pressedSquare(4)
 
 
-//var game3 = TicTacToeGame()
-//game3.gameBoard = [.x, .x, .o,
-//    .o, .o, .x,
-//    .x, .o, .none]
+var game3 = TicTacToeGame()
+game3.gameBoard = [.x, .x, .o,
+    .o, .o, .x,
+    .x, .o, .none]
 //game3.pressedSquare(8)
 
