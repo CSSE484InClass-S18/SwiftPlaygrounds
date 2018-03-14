f27b70dd6716faf0bcfb947cf52a6ee595ef56fa
David Fisher <fisherds@rose-hulman.edu>
2018-03-14 11:57:58 -0400

    TicTacToeGame

--- follow alongs/TicTacToeGame.playground/contents.swift
+++ follow alongs/TicTacToeGame.playground/contents.swift
@@ -28,6 +28,44 @@ class TicTacToeGame: CustomStringConvertible {
     return gameString
   }
 
+  func pressedSquare(_ index: Int) {
+    if gameBoard[index] != .none {
+      return // This square is not empty
+    }
+    if gameState == .xTurn {
+      gameBoard[index] = .x
+      gameState = .oTurn
+      checkForGameOver()
+    } else if gameState == .oTurn {
+      gameBoard[index] = .o
+      gameState = .xTurn
+      checkForGameOver()
+    }
+  }
+
+  func checkForGameOver() {
+    if !gameBoard.contains(.none) {
+      gameState = .tie
+    }
+
+    var linesOf3 = [String]()
+    linesOf3.append(getGameString([0, 1, 2]))
+    linesOf3.append(getGameString([3, 4, 5]))
+    linesOf3.append(getGameString([6, 7, 8]))
+    linesOf3.append(getGameString([0, 3, 6]))
+    linesOf3.append(getGameString([1, 4, 7]))
+    linesOf3.append(getGameString([2, 5, 8]))
+    linesOf3.append(getGameString([0, 4, 8]))
+    linesOf3.append(getGameString([2, 4, 6]))
+    for lineOf3 in linesOf3 {
+      if lineOf3 == "XXX" {
+        gameState = .xWon
+      } else if lineOf3 == "OOO" {
+        gameState = .oWon
+      }
+    }
+  }
+
   var description: String {
     return "\(gameState.rawValue)  Board: \(getGameString())"
   }
@@ -36,24 +74,28 @@ class TicTacToeGame: CustomStringConvertible {
 
 /* ----------------- Official Playground testing ----------------- */
 var game = TicTacToeGame()
-//game.pressedSquare(0)
-//game.pressedSquare(1)
-//game.pressedSquare(3)
-//game.pressedSquare(2)
-//game.pressedSquare(6)
-
+game.pressedSquare(0)
+game.pressedSquare(1)
+game.pressedSquare(1)
+game.pressedSquare(1)
+game.pressedSquare(1)
+game.pressedSquare(1)
+game.pressedSquare(3)
+game.pressedSquare(2)
+game.pressedSquare(6)
+game.pressedSquare(8)
 
 var game2 = TicTacToeGame()
 game2.gameBoard = [.x, .x, .o,
                 .none, .none, .none,
                 .o, .none, .none]
-//game2.pressedSquare(8)
-//game2.pressedSquare(4)
+game2.pressedSquare(8)
+game2.pressedSquare(4)
 
 
 var game3 = TicTacToeGame()
 game3.gameBoard = [.x, .x, .o,
     .o, .o, .x,
     .x, .o, .none]
-//game3.pressedSquare(8)
+game3.pressedSquare(8)
 
